b94789bb8c6c1b8dfcb93cfa817d2759024ab3ce
eric <61746940+M-erick@users.noreply.github.com>
2023-03-13 21:21:51 +0300

    Removed the gallery tab in menu component

--- Menu Bar.php
+++ Menu Bar.php
@@ -20,7 +20,7 @@ error_reporting(1);
       <ul class="nav navbar-nav">
         <li><a href="index.php"title="Home">Home</a></li>
         <li><a href="about.php"title="About">About </a></li>
-		    <li><a href="image gallery.php"title="Gallery">Gallery </a></li>
+		    <!-- <li><a href="image gallery.php"title="Gallery">Gallery </a></li> -->
       </ul>
       <ul class="nav navbar-nav navbar-right">
         

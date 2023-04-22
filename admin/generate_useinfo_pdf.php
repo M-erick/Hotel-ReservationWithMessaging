78ac7688455ae98c0565eeee2e2ea6ea56b3c5cc
eric <61746940+M-erick@users.noreply.github.com>
2023-04-22 18:08:47 +0300

    update user information details

--- admin/generate_useinfo_pdf.php
+++ admin/generate_useinfo_pdf.php
@@ -34,7 +34,25 @@ $pdf->Image('logo12.png', $imageX, 10, $imageWidth);
 // Add an empty cell for spacing
 $pdf->Cell(0, 15, '', 0, 1);
 
+
+// Add email and contact information to the top right corner
+$pdf->SetXY($pageWidth - 100, 10);
+$pdf->SetFont('times', 'B', 10);
+$pdf->Cell(95, 5, 'Email: E&M@gmail.com | Contact: (+254)713131313', 0, 1, 'R');
+
+
+$pdf->SetXY(10, 10);
+$pdf->SetFont('times', 'B', 10);
+$pdf->Cell(95, 5, 'PO Box 2375, Nairobi', 0, 1, 'L');
+
+$pdf->SetFont('times', '', 12);
+
+// Add an empty cell for spacing
+
+$pdf->Cell(0, 15, '', 0, 1);
+
 // Add a heading
+$pdf->SetFont('times', 'B', 16);
 $pdf->Cell(0, 10, 'User Registration Details', 0, 1, 'C');
 
 // Set font size
@@ -43,14 +61,13 @@ $pdf->SetFont('times', '', 12);
 // Create a table
 $html = '<table border="1">
             <tr>
-                <th>Sr No</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Password</th>
-                <th>Mobile</th>
-                <th>Address</th>
-                <th>Gender</th>
-                <th>Country</th>
+                <th><strong>Sr No</strong></th>
+                <th><strong>Name</strong></th>
+                <th><strong>Email</strong></th>
+                <th><strong>Mobile</strong></th>
+                <th><strong>Address</strong></th>
+                <th><strong>Gender</strong></th>
+                <th><strong>Country</strong></th>
             </tr>';
 
 $i=1;
@@ -61,7 +78,6 @@ while($res=mysqli_fetch_assoc($sql))
                 <td>'. $i .'</td>
                 <td>'. $res['name'] .'</td>
                 <td>'. $res['email'] .'</td>
-                <td>'. $res['password'] .'</td>
                 <td>'. $res['mobile'] .'</td>
                 <td>'. $res['address'] .'</td>
                 <td>'. $res['gender'] .'</td>
@@ -72,6 +88,13 @@ while($res=mysqli_fetch_assoc($sql))
 
 $html .= '</table>';
 $pdf->writeHTML($html);
+
+// Add date to the PDF
+date_default_timezone_set('Africa/Nairobi');
+$now = date('Y-m-d H:i:s');
+$pdf->SetFont('times','B',12);
+$pdf->Cell(0, 10, 'Created on: ' . $now, 0, 0, 'R');
+$pdf->SetFont('times','',12);
 
 // Output the PDF to the browser
 $pdf->Output('user_registration_details.pdf', 'D');

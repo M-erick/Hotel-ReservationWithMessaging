8f54e20a8aa660b3f93bc7bead54cbcf5f88403a
eric <61746940+M-erick@users.noreply.github.com>
2023-04-22 07:16:46 +0300

    update the status data from pending to booked

---
+++ admin/update_order.php
@@ -0,0 +1,10 @@
+<?php 
+include('../connection.php');
+$oid=$_GET['booking_id'];
+$booked = 'booked';
+$q=mysqli_query($con,"update  room_booking_details set  status = '$booked' where id='$oid' ");
+if($q)
+{
+header('location:dashboard.php?option=booking_details');
+}
+?>

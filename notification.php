6c6953d9e37f983644b570ce6532187f4562695b
eric <61746940+M-erick@users.noreply.github.com>
2023-04-27 06:12:23 +0300

    finished with  the notification part

--- notification.php
+++ notification.php
@@ -97,47 +97,33 @@ mysqli_query($con, $sql);
   <div class="container">
 <?php
 $sql = mysqli_query($con, "SELECT * FROM feedback WHERE email='$eid'");
-$messages = array(); // initialize the messages array
-while ($res = mysqli_fetch_assoc($sql)) {
-    $message_id = $res['id'];
-    $message_text = $res['message'];
-    $response_text = $res['response'];
-    $date_time = date('F j, Y g:i a', strtotime($res['date_time']));
-    $response_time = date('F j, Y g:i a', strtotime($res['response_time']));
-
-    // check if the message ID exists in the messages array
-    if (array_key_exists($message_id, $messages)) {
-        // if it does, update the response text and time
-        $messages[$message_id]['response'] = $response_text;
-        $messages[$message_id]['response_time'] = $response_time;
-    } else {
-        // if it doesn't, add the message to the array
-        $messages[$message_id] = array(
-            'message' => $message_text,
-            'date_time' => $date_time,
-            'response' => $response_text,
-            'response_time' => $response_time
-        );
-    }
-}
-
-// loop through the messages array to display the messages
-foreach ($messages as $message) {
-?>
-    <div class="message-container">
+if (mysqli_num_rows($sql) > 0) {
+    while ($res = mysqli_fetch_assoc($sql)) {
+      $message_id = $res['cid'];
+      $message_text = $res['message'];
+      $response_text = $res['response'];
+    //   $date_time = $res['date_time'];
+    //   $response_time = $res['response_time'];
+      $response_time = date('M j, Y, g:i a', strtotime($res['response_time']));
+      $date_time = date('M j, Y, g:i a', strtotime($res['date_time']));
+  ?>
+      <div class="message-container">
         <div class="message message-left">
-            <p><?php echo $message['message']; ?></p>
-            <span class="time"><?php echo $message['date_time']; ?></span>
+          <p><?php echo $message_text; ?></p>
+          <span class="time"><?php echo $date_time; ?></span>
         </div>
-        <?php if ($message['response']) { ?>
-            <div class="message message-right">
-                <p><?php echo $message['response']; ?></p>
-                <span class="time"><?php echo $message['response_time']; ?></span>
-            </div>
+        <?php if ($response_text) { ?>
+          <div class="message message-right">
+            <p><?php echo $response_text; ?></p>
+            <span class="time"><?php echo $response_time; ?></span>
+          </div>
         <?php } ?>
-    </div>
-<?php
-} // end foreach
+      </div>
+  <?php
+    } // end while
+  } else {
+    echo "<p>No messages found.</p>";
+  }
 ?>
 </div>
 

e6e5beaf14d627c9a0b8171b2b3c1b2c6b26f0c0
eric <61746940+M-erick@users.noreply.github.com>
2023-05-06 06:01:12 +0300

    title update

--- notification.php
+++ notification.php
@@ -15,7 +15,7 @@ mysqli_query($con, $sql);
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Online Hotel.com</title>
+  <title>E & M Hotel</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
